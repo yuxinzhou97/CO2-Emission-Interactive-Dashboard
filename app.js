05b60d3309baf0219ac4f34dd5bbaff0f00c8056
Yuxin Zhou <yuxinzhou1997@gmail.com>
2020-08-03 01:18:38 -0700

    create and draw world map

---
+++ app.js
@@ -0,0 +1,49 @@
+d3.queue()
+  .defer(d3.json, "//unpkg.com/world-atlas@1.1.4/world/50m.json")
+  .defer(d3.csv, "./data/all_data.csv", function(row) {
+    return {
+      continent: row.Continent,
+      country: row.Country,
+      countryCode: row["Country Code"],
+      emissions: +row["Emissions"],
+      emissionsPerCapita: +row["Emissions Per Capita"],
+      region: row.Region,
+      year: +row.Year
+    }
+  })
+
+
+  .await(function(error, mapData, data) {
+    if (error) throw error;
+
+    var extremeYears = d3.extent(data, d => d.year);
+    var currentYear = extremeYears[0];
+    var currentDataType = d3.select('input[name="data-type"]:checked')
+                            .attr("value");
+    var geoData = topojson.feature(mapData, mapData.objects.countries).features;
+
+    var width = +d3.select(".chart-container")
+                   .node().offsetWidth;
+
+
+    createMap(width, width * 4 / 5);
+    drawMap(geoData, data, currentYear, currentDataType);
+
+    d3.select("#year")
+        .property("min", currentYear)
+        .property("max", extremeYears[1])
+        .property("value", currentYear)
+        .on("input", () => {
+          currentYear = +d3.event.target.value;
+          drawMap(geoData, data, currentYear, currentDataType);
+        });
+
+    d3.selectAll('input[name="data-type"]')
+        .on("change", () => {
+          var active = d3.select(".active").data()[0];
+          var country = active ? active.properties.country : "";
+          currentDataType = d3.event.target.value;
+          drawMap(geoData, data, currentYear, currentDataType);
+        });
+
+  });
